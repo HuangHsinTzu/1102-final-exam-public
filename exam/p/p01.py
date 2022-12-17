3875ba0f292102276d1ab3bd63f5df7258dbdbae
Sandy Huang <sandyhuang1223@gmail.com>
2022-12-17 20:47:06 +0800

    commit

--- exam/p/p01.py
+++ exam/p/p01.py
@@ -1,6 +1,8 @@
 def p01(*nums):
     minnum=None
     # ↓程式區域↓
-
+    input = [*nums]
+    input.sort()
+    minnum = input[0]
     # ↑程式區域↑
     return minnum
--- exam/p/p02.py
+++ exam/p/p02.py
@@ -1,6 +1,10 @@
 def p02(a=1, b=100):
     evensum=None
+    result = 0
     # ↓程式區域↓
-
+    for i in range(a,b+1):
+        if i % 2 == 0:
+            result += i
+            evensum = result
     # ↑程式區域↑
     return evensum
