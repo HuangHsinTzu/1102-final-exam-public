4d119d690eda61346c3d275ce8d5bada9954e617
Sandy Huang <sandyhuang1223@gmail.com>
2022-12-22 20:49:33 +0800

    update

--- exam/p/p07.py
+++ exam/p/p07.py
@@ -1,6 +1,37 @@
 def p07(version1='1.0.0', version2='1.0.0'):
     output=None
     # ↓程式區域↓
-
+    if len(version2) == len(version1):
+        version1_list = []
+        version2_list = []
+        # 將input放進version1_list & version2_list:
+        version1_list = version1.split('.')
+        version2_list = version2.split('.')
+        # version1_list和version2_list的比較(從index 0 到 index 1):
+        for i in range(0, len(version1_list)):
+            if int(version1_list[0]) < int(version2_list[0]): # index0
+                output = -1
+                break
+            elif int(version1_list[0]) > int(version2_list[0]):
+                output = 1
+                break
+            else: # version1_list[0] == version2_list[0], 比較index1
+                if int(version1_list[1]) < int(version2_list[1]):
+                    output = -1
+                    break
+                elif int(version1_list[1]) > int(version2_list[1]):
+                    output = 1
+                    break
+                else: # version1_list[1] == version2_list[1], 比較index2
+                    if int(version1_list[2]) < int(version2_list[2]):
+                        output = -1
+                        break
+                    elif int(version1_list[2]) > int(version2_list[2]):
+                        output = 1
+                        break
+                    output = 0
+    else:
+        output = 0
     # ↑程式區域↑
     return output
+print(p07())
--- exam/p/p08.py
+++ exam/p/p08.py
@@ -1,6 +1,16 @@
-def p08(n=12):
+def p08(n=13):
     output=None
     # ↓程式區域↓
-
+    squar = 1
+    i = 0
+    while ((i+1)**2)*2 < n :
+        i += 1
+        squar = i**2 
+    if n % squar == 0:
+        output = n//squar
+    else:
+        squar = (i+1)**2
+        output = n//squar + 1
     # ↑程式區域↑
     return output
+print(p08())
