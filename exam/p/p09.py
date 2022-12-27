d87d67c8479e9cc2c489d87082f158820693c570
Sandy Huang <sandyhuang1223@gmail.com>
2022-12-27 23:23:06 +0800

    update

--- exam/p/p09.py
+++ exam/p/p09.py
@@ -1,6 +1,14 @@
-def p09(citations=[]):
+def p09(citations=[1,3,1]):
     h=None
     # ↓程式區域↓
-
+    for i in range(1, len(citations)+1):
+        count = 0
+        for j in range(0, len(citations)):
+            if citations[j] >= i :
+                count = count+1
+            if count == i :
+                h = count
+                break
     # ↑程式區域↑
     return h
+print(p09())
