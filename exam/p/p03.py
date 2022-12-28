6a6d3c0358d7f8f81cf02330ecbefe57dfccd615
Sandy Huang <sandyhuang1223@gmail.com>
2022-12-28 12:44:26 +0800

    update

--- exam/p/p03.py
+++ exam/p/p03.py
@@ -1,10 +1,10 @@
 def p03(x=210):
     reverse=None
     # ↓程式區域↓
+    output = 0
     input = str(x)
-    reverse = input[::-1]
-    if(reverse[0] == '0'):
-        reverse = reverse[1,len(reverse)]
+    output = input[::-1]
+    reverse = int(output)
     # ↑程式區域↑
     return reverse
 print(p03())
